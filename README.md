116929a3a92687f4416f302c4d8326538db5767b
Toni Mäkinen <masfonos@gmail.com>
2021-01-12 16:28:08 +0200

    Update project

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # TinyPE
 POC Tiny PE - Visual Studio 2019 project
 
-- 960 bytes (32-bit) (944 with reloc stripped)
+- 944 bytes (32-bit) (928 with reloc stripped)
 - 992 bytes (64-bit)
 
 
--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@
 #pragma comment(linker, "/MERGE:.text=.tinype")
 #pragma comment(linker, "/MERGE:.rdata=.tinype")
 #pragma comment(linker, "/MERGE:.pdata=.tinype")
-
 #endif
 
 
@@ -17,27 +16,6 @@
 
 void mainCRTStartup()
 {
-#if 0
-	HMODULE hModule = GetModuleHandle(nullptr);
-	if(!hModule) return;
-
-	// Get DOS header
-	IMAGE_DOS_HEADER *pDOSHeader = (IMAGE_DOS_HEADER *)hModule;
-
-	// Get PE header
-	IMAGE_NT_HEADERS *pPEHeader = (IMAGE_NT_HEADERS *)((BYTE *)pDOSHeader + pDOSHeader->e_lfanew);
-
-	struct DOS_STUB
-	{
-		BYTE header[64];
-	};
-
-	// DOS Stub, 64 bytes from module start
-	DOS_STUB *pDOSStub = (DOS_STUB *)((BYTE *)pDOSHeader + 64);
-
-	// PE - DosHeader+64 = Stub + Rich 
-#endif
-
 
 	DWORD written{ 0 };
 	WriteFile(GetStdHandle(STD_OUTPUT_HANDLE), "Hello World", 12, &written, nullptr);
