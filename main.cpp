6b57c61f641a1f1789a22f07fe7eaf596e2c73f2
Toni Mäkinen <masfonos@gmail.com>
2021-01-12 16:03:25 +0200

    32-bit, fixed base address to no

--- main.cpp
+++ main.cpp
@@ -4,15 +4,41 @@
 #define STRICT
 #include <Windows.h>											   
 
+#ifndef _DEBUG
 #pragma comment(linker, "/ALIGN:16")
-#pragma comment(linker, "/MERGE:.rdata=.text")
-#pragma comment(linker, "/MERGE:.pdata=.text")
+#pragma comment(linker, "/MERGE:.text=.tinype")
+#pragma comment(linker, "/MERGE:.rdata=.tinype")
+#pragma comment(linker, "/MERGE:.pdata=.tinype")
+
+#endif
 
 
 
 
 void mainCRTStartup()
 {
+#if 0
+	HMODULE hModule = GetModuleHandle(nullptr);
+	if(!hModule) return;
+
+	// Get DOS header
+	IMAGE_DOS_HEADER *pDOSHeader = (IMAGE_DOS_HEADER *)hModule;
+
+	// Get PE header
+	IMAGE_NT_HEADERS *pPEHeader = (IMAGE_NT_HEADERS *)((BYTE *)pDOSHeader + pDOSHeader->e_lfanew);
+
+	struct DOS_STUB
+	{
+		BYTE header[64];
+	};
+
+	// DOS Stub, 64 bytes from module start
+	DOS_STUB *pDOSStub = (DOS_STUB *)((BYTE *)pDOSHeader + 64);
+
+	// PE - DosHeader+64 = Stub + Rich 
+#endif
+
+
 	DWORD written{ 0 };
 	WriteFile(GetStdHandle(STD_OUTPUT_HANDLE), "Hello World", 12, &written, nullptr);
 }												
