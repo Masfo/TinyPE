d4a990425a12efa152fc3d6df1ff6ca79728bd90
Toni Mäkinen <masfonos@gmail.com>
2021-06-27 14:13:13 +0300

    Formatting

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #pragma warning(disable : 5039)  // warning C5105: pointer or reference to potentially throwing function passed to
-				 // 'extern "C"' function under -EHc.
+                                 // 'extern "C"' function under -EHc.
 
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
@@ -8,7 +8,7 @@
 #include <cstdint>
 
 #define CONSOLE 0		// 1 - console application
-		                // 0 - Windows application
+                        // 0 - Windows application
 
 #pragma comment(linker, "/ALIGN:16")
 #ifndef _DEBUG
@@ -23,8 +23,8 @@
 
 void mainCRTStartup()
 {
-	DWORD written{ 0 };
-	WriteFile(GetStdHandle(STD_OUTPUT_HANDLE), "Hello", 6, &written, nullptr);
+    DWORD written{ 0 };
+    WriteFile(GetStdHandle(STD_OUTPUT_HANDLE), "Hello", 6, &written, nullptr);
 }
 
 #else
@@ -32,6 +32,6 @@ void mainCRTStartup()
 
 void WinMainCRTStartup()
 {
-	MessageBoxA(nullptr, "Hello", "Hello", MB_OK | MB_ICONASTERISK);
+    MessageBoxA(nullptr, "Hello", "Hello", MB_OK | MB_ICONASTERISK);
 }
 #endif
