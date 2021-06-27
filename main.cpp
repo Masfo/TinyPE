70a8047affa70ece8795a4570bebfd57020d5f34
Toni Mäkinen <masfonos@gmail.com>
2021-06-27 18:40:08 +0300

    winmain.cpp:   - Forgot to update subsystem   - Moved pragmas on top
    Project: renamed "Release/Debug Windows" to "Release/Debug Gui"

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
+
+#ifdef _WIN64
 #pragma warning(disable : 5039)  // warning C5105: pointer or reference to potentially throwing function passed to
                                  // 'extern "C"' function under -EHc.
+#endif
+
+#pragma comment(linker, "/ALIGN:16")
+#pragma comment(linker, "/SUBSYSTEM:CONSOLE")
+#ifndef _DEBUG
+#pragma comment(linker, "/MERGE:.rdata=.text")
+#pragma comment(linker, "/MERGE:.pdata=.text")
+#else
+#endif
 
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
@@ -8,12 +19,6 @@
 #include <cstdint>
 
 
-#pragma comment(linker, "/ALIGN:16")
-#pragma comment(linker, "/SUBSYSTEM:CONSOLE")
-#ifndef _DEBUG
-#pragma comment(linker, "/MERGE:.rdata=.text")
-#pragma comment(linker, "/MERGE:.pdata=.text")
-#endif
 
 
 
