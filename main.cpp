038d2288b01f79e10cf25ad4aba46d1e492772c3
Toni Mäkinen <masfonos@gmail.com>
2021-07-06 05:19:45 +0300

    Added .clang-format, also format all

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 
-#pragma warning(disable : 5039)  // warning C5105: pointer or reference to potentially throwing function passed to
-                                 // 'extern "C"' function under -EHc.
+#pragma warning(disable : 5039)   // warning C5105: pointer or reference to potentially throwing function passed to
+                                  // 'extern "C"' function under -EHc.
 
 #pragma comment(linker, "/ALIGN:16")
 #pragma comment(linker, "/SUBSYSTEM:CONSOLE")
 
 #ifndef _DEBUG
-#pragma comment(linker, "/MERGE:.rdata=.text")
-#pragma comment(linker, "/MERGE:.pdata=.text")
+#    pragma comment(linker, "/MERGE:.rdata=.text")
+#    pragma comment(linker, "/MERGE:.pdata=.text")
 #endif
 
 #define WIN32_LEAN_AND_MEAN
@@ -17,12 +17,8 @@
 #include <cstdint>
 
 
-
-
-
 void mainCRTStartup()
 {
-    DWORD written{ 0 };
+    DWORD written{0};
     WriteFile(GetStdHandle(STD_OUTPUT_HANDLE), "Hello", 5, &written, nullptr);
 }
-
--- winmain.cpp
+++ winmain.cpp
@@ -1,13 +1,13 @@
 
-#pragma warning(disable : 5039)  // warning C5105: pointer or reference to potentially throwing function passed to
-                                 // 'extern "C"' function under -EHc.
+#pragma warning(disable : 5039)   // warning C5105: pointer or reference to potentially throwing function passed to
+                                  // 'extern "C"' function under -EHc.
 
 #pragma comment(linker, "/ALIGN:16")
 #pragma comment(linker, "/SUBSYSTEM:WINDOWS")
 
 #ifndef _DEBUG
-#pragma comment(linker, "/MERGE:.rdata=.text")
-#pragma comment(linker, "/MERGE:.pdata=.text")
+#    pragma comment(linker, "/MERGE:.rdata=.text")
+#    pragma comment(linker, "/MERGE:.pdata=.text")
 
 #endif
 
@@ -16,8 +16,6 @@
 #define STRICT
 #include <Windows.h>
 #include <cstdint>
-
-
 
 
 void WinMainCRTStartup()
