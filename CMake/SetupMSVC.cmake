d3861abc422618c8648b9b43b7821b69ccc05131
Toni Mäkinen <masfonos@gmail.com>
2024-07-20 16:10:02 +0300

    Remove /Za option, because of windows header anonymous structs, it worked before, something changed between last test.
    Tested on 22H2, 22621.3880

--- CMake/SetupMSVC.cmake
+++ CMake/SetupMSVC.cmake
@@ -74,16 +74,14 @@ function(setup_tiny_pe target desktop outputname include_dir)
     target_compile_options(${target} PRIVATE /Zc:__cplusplus /Zc:alignedNew /Zc:checkGwOdr)
     target_compile_options(${target} PRIVATE /utf-8)
     target_compile_options(${target} PRIVATE /EHsc)
-    target_compile_options(${target} PRIVATE /Za)
+    #target_compile_options(${target} PRIVATE /Za)
 
     target_compile_options(${target} PRIVATE /fp:precise)
     target_compile_options(${target} PRIVATE /diagnostics:caret)
 
-
-    target_compile_options(${target} PRIVATE /experimental:module)
     target_compile_options(${target} PRIVATE /Zc:preprocessor)
     target_compile_options(${target} PRIVATE /permissive-)
-    #target_compile_options(${target} PRIVATE /std:c++latest)
+    target_compile_options(${target} PRIVATE /std:c++latest)
 
     target_compile_options(${target} PRIVATE /Wall)
     # target_compile_options(${target} PRIVATE /WX) # Warnings as errors
