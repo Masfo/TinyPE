c2809c26d6ab4d5b3311e391c70d4582633aff2e
Toni Mäkinen <masfonos@gmail.com>
2025-08-09 20:13:11 +0300

    seed with tickcount

--- src/main.cpp
+++ src/main.cpp
@@ -14,23 +14,26 @@ constexpr unsigned int seed_from_time(const char* t)
 	return (t[0] - '0') * 36000u + (t[1] - '0') * 3600u + (t[3] - '0') * 600u + (t[4] - '0') * 60u + (t[6] - '0') * 10u + (t[7] - '0');
 }
 
-static unsigned int seed = seed_from_time(__TIME__);
+static unsigned int gseed = seed_from_time(__TIME__);
+
+void seed(unsigned int s) { gseed = (gseed << 16 | gseed >> 16) ^ s; }
 
 unsigned int rand()
 {
-	seed = seed * 1664525 + 1013904223;
-	return seed;
+	gseed = gseed * 1664525 + 1013904223;
+	return gseed;
 }
-
 
 void maincrt()
 {
 #if defined(TINY_GUI)
 #else
 
-	 auto cmdline = GetCommandLineA();
+	seed(GetTickCount());
 
-	unsigned int parsed_size = 4; 
+	auto cmdline = GetCommandLineA();
+
+	unsigned int parsed_size = 4;
 	const char*  arg         = cmdline;
 
 	// Skip program name
@@ -51,7 +54,6 @@ void maincrt()
 	}
 
 
-
 	unsigned int size = 1024 * parsed_size;
 
 	unsigned int chars_per_line = 0;
@@ -61,22 +63,19 @@ void maincrt()
 		WriteFile(GetStdHandle(STD_OUTPUT_HANDLE), &c, 1, nullptr, 0);
 
 		chars_per_line++;
-		if (chars_per_line >= 50+(rand()%40))
+		if (chars_per_line >= 50 + (rand() % 40))
 		{
 			WriteFile(GetStdHandle(STD_OUTPUT_HANDLE), "\n", 1, nullptr, 0);
-
-				size--;
+			size--;
 
 			chars_per_line = 0;
 
-		if (rand() % 10 == 0)
+			if (rand() % 10 == 0)
 			{
 				WriteFile(GetStdHandle(STD_OUTPUT_HANDLE), "\n", 1, nullptr, 0);
 				size--;
 			}
 		}
-
-
 	}
 #endif
 }
