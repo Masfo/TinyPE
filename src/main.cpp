16f5ad23e4cca6391b8011089cac482cb05636d4
Toni Mäkinen <masfonos@gmail.com>
2025-08-09 19:14:23 +0300

    lines

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,6 @@
 
 constexpr unsigned int seed_from_time(const char* t)
 {
-	// t: "hh:mm:ss"
 	return (t[0] - '0') * 36000u + (t[1] - '0') * 3600u + (t[3] - '0') * 600u + (t[4] - '0') * 60u + (t[6] - '0') * 10u + (t[7] - '0');
 }
 
@@ -31,11 +30,25 @@ void maincrt()
 #if defined(TINY_GUI)
 	MessageBoxA(nullptr, message, message, MB_OK);
 #else
-	unsigned int size = 1024 * 1024; // 1 MB
-	for (int i = 0; i < size; ++i)
+	unsigned int size = 1024 * 16;
+
+	unsigned int chars_per_line = 0;
+	for (unsigned int i = 0; i < size; ++i)
 	{
-		char c = static_cast<char>((rand() % 95) + 32); // Map to printable ASCII characters
+		char c = static_cast<char>((rand() % 95) + 32);
 		WriteFile(GetStdHandle(STD_OUTPUT_HANDLE), &c, 1, nullptr, 0);
+
+		chars_per_line++;
+		if (chars_per_line >= 50+(rand()%40))
+		{
+			WriteFile(GetStdHandle(STD_OUTPUT_HANDLE), "\n", 1, nullptr, 0);
+			chars_per_line = 0;
+
+		if(rand() % 10 == 0)
+			WriteFile(GetStdHandle(STD_OUTPUT_HANDLE), "\n", 1, nullptr, 0);
+		}
+
+
 	}
 #endif
 }
